ab2ccaeced768eab386ec31ae2bc31fa377ed8c0
Michael Schmitt <schmitt.michael@funct.com>
2016-04-19 10:58:57 +0200

    namespace

--- config/module.config.php
+++ config/module.config.php
@@ -1,8 +1,6 @@
 <?php
 return array(
-
     'service_manager' => array(
-
         'delegators' => array(
             'ZF\MvcAuth\Authentication\DefaultAuthenticationListener' => array(
                 Schmiddim\MvcAuth\Factory\AuthenticationAdapterDelegatorFactory::class,
